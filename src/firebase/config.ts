f442e9ba9fc0a40441f45413fc460b8e0d408281
Asfand Ahmed k23-0698 <k230698@nu.edu.pk>
2025-10-18 21:30:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2605668099-3ce41",
+  "appId": "1:876375808347:web:c70f7257ecf497a53ce27e",
+  "apiKey": "AIzaSyAjD4emwidPDdpg9Q3XNgCI5Ar4ADtno90",
+  "authDomain": "studio-2605668099-3ce41.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "876375808347"
+};
